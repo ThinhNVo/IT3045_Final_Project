ddb844afbd11c0b2cd9dda47173e462a0feace3a
beechaah <beechaah@mail.uc.edu>
2025-12-04 12:57:14 -0500

    Update Courses Migration Content

--- Group4_IT3045_FinalProject/Group4_IT3045_FinalProject/Migrations/20251130162406_CourseTable.cs
+++ Group4_IT3045_FinalProject/Group4_IT3045_FinalProject/Migrations/20251130162406_CourseTable.cs
@@ -10,13 +10,28 @@ namespace Group4_IT3045_FinalProject.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.CreateTable(
+                name: "Course",
+                columns: table => new
+                {
+                    CourseID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Credits = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Availability = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Course", x => x.CourseID);
+                });
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.DropTable(
+                name: "Course");
         }
     }
 }
